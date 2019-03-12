781c7f5a9c1696a0c57fac3c096ffa98307767f8
ramongonze <ramongonze@gmail.com>
2019-03-12 15:50:40 -0300

    Updated the documentation. Created tutorial for Distributions

--- html/menudata.js
+++ html/menudata.js
@@ -1,5 +1,5 @@
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Classes",url:"annotated.html"},
+{text:"Home",url:"index.html"},
 {text:"Functions",url:"pages.html"},
-{text:"Files",url:"files.html"}]}
+{text:"Classes",url:"annotated.html"},
+{text:"Tutorial",url:"files.html"}]}
